8e9463406038f46a79f3241cdac3bf099c66b0fd
Robben98744 <43476581+Robben98744@users.noreply.github.com>
2018-09-22 01:22:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,4 +15,13 @@ if (message.content === '!spam') {
       }
 });
 
+client.on('message',function(message) {
+    let prefix = "-";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
 client.login(process.env.BOT_TOKEN);
