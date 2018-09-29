bac9d35e793b5e728f8ae59e2e1c58796c326da0
Robben98744 <43476581+Robben98744@users.noreply.github.com>
2018-09-29 02:10:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,4 +15,24 @@ if (message.content === '!spam') {
       }
 });
 
+const = ['.']
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "say" + "<@" + `${client.user.id}` + ">") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
